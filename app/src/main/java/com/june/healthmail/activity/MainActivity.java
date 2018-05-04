f45a035f22ae8e3e033d876bec6cbd6d3163a057
胡军杰 <1006879674@qq.com>
2018-05-04 18:31:07 +0800

    增加权限提醒

--- app/src/main/java/com/june/healthmail/activity/MainActivity.java
+++ app/src/main/java/com/june/healthmail/activity/MainActivity.java
@@ -1,6 +1,16 @@
 package com.june.healthmail.activity;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
@@ -41,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
   };
 
   private String[] mTabSpecs = new String[]{"猫友圈","功能列表","小号管理","个人中心" };
+  private static final int MY_PERMISSION_REQUEST_CODE = 10000;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -85,6 +96,8 @@ public class MainActivity extends AppCompatActivity {
         }
       }
     });
+
+    checkPermission();
   }
 
   private void addTab() {
@@ -134,5 +147,96 @@ public class MainActivity extends AppCompatActivity {
     if(index <= 3){
       mTabHost.setCurrentTab(0);
     }
+  }
+
+
+  private void checkPermission() {
+    /**
+     * 第 1 步: 检查是否有相应的权限
+     */
+    boolean isAllGranted = checkPermissionAllGranted(
+      new String[] {
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+      }
+    );
+
+    //如果两个权限都有，正常进行
+    if (isAllGranted) {
+
+    }else {
+      /**
+       * 第 2 步: 请求权限
+       */
+      // 一次请求多个权限, 如果其他有权限是已经授予的将会自动忽略掉
+      ActivityCompat.requestPermissions(
+        this,
+        new String[] {
+          Manifest.permission.WRITE_EXTERNAL_STORAGE
+        },
+        MY_PERMISSION_REQUEST_CODE
+      );
+    }
+  }
+
+  /**
+   * 检查是否拥有指定的所有权限
+   */
+  private boolean checkPermissionAllGranted(String[] permissions) {
+    for (String permission : permissions) {
+      if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+        // 只要有一个权限没有被授予, 则直接返回 false
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**
+   * 第 3 步: 申请权限结果返回处理
+   */
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    if (requestCode == MY_PERMISSION_REQUEST_CODE) {
+      boolean isAllGranted = true;
+      // 判断是否所有的权限都已经授予了
+      for (int grant : grantResults) {
+        if (grant != PackageManager.PERMISSION_GRANTED) {
+          isAllGranted = false;
+          break;
+        }
+      }
+
+      if (isAllGranted) {
+        // 如果所有的权限都授予了, 则执行备份代码
+      } else {
+        // 弹出对话框告诉用户需要权限的原因, 并引导用户去应用权限管理中手动打开权限按钮
+        openAppDetails();
+      }
+    }
+  }
+
+  /**
+   * 打开 APP 的详情设置
+   */
+  private void openAppDetails() {
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    builder.setMessage("在线更新需要手机存储权限，请到 “应用信息 -> 权限” 中授予！");
+    builder.setPositiveButton("去手动授权", new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setData(Uri.parse("package:" + getPackageName()));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+        startActivity(intent);
+      }
+    });
+    builder.setNegativeButton("取消", null);
+    builder.show();
   }
 }
